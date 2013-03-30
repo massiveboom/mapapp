6535e64f60f6967e55c7f9f3a18f839f1da20dd2
devnill <devnill@gmail.com>
2013-03-30 00:54:09 -0600

    object skeleton

---
+++ public/javascripts/map.js
@@ -0,0 +1,25 @@
+var map={
+    init:function(){},
+    utils:{//commonly repeated code
+	
+    },
+    canvas:{
+	//the actual map
+    },
+    geoip:{
+	//geolocation functions
+    }
+    markers:{
+	//markers on the map
+	
+    },
+    route:{
+	//routes on the map
+	display,
+	service,
+	calculate,	
+    }
+    server:{
+	//ajax functions for phoning home
+    }
+}
