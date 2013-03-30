5a0d90926d1ad846519cfcc4f12f5a95d6455310
devnill <devnill@gmail.com>
2013-03-30 16:47:31 -0600

    added address encoder

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,23 +6,29 @@ $(document).ready(function(){
 	    map.geocoder = new google.maps.Geocoder();
 	    
 	    //event listeners
-	    map.listener.click=google.maps.event.addListener(map.canvas, 'click', function(e) {
+	    map.listener.mapClick=google.maps.event.addListener(map.canvas, 'click', function(e) {
 		console.log(['click',e]);
 	    });
-	    
-
-
-
-
-	    
-	},	
+	},
 	listener:{},
 	utils:{//commonly repeated code
-		loadData: function() {},
-		loadAll: function() {},
-		
-		clearData: function() {},
-		clearAll: function() {}
+	    loadData: function() {},
+	    loadAll: function() {},
+	    
+	    clearData: function() {},
+	    clearAll: function() {},
+            encodeAddress:function(location,callback){
+                map.geocoder.geocode( { 'address': location}, function(results, status) {
+      		    if(status != google.maps.GeocoderStatus.OK){
+			console.log("lookup encoded is:"+results[0].geometry.location);	
+	                results=false;
+      		    } 
+                    if(callback){
+                        callback(results);
+                    }
+                });
+            }
+            
 	},
 	markers:{
 	    //markers on the map
@@ -39,17 +45,17 @@ $(document).ready(function(){
 	    //variables that store preferences
 	    mapOptions : {
 		zoom: 4,
-   		center: new google.maps.LatLng(37.09024, -95.712891),
-   		mapTypeId: google.maps.MapTypeId.TERRAIN
-   	    },
+		center: new google.maps.LatLng(37.09024, -95.712891),
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	    },
 	    sampleData: ['/sample data/6800 E Tennessee Ave.kml',
-                         '/sample data/6800 E Tennessee Ave(2).kml',
-                         '/sample data/6800 E Tennessee Ave(3).kml',
-                         '/sample data/6800 E Tennessee Ave(4).kml',
+			 '/sample data/6800 E Tennessee Ave(2).kml',
+			 '/sample data/6800 E Tennessee Ave(3).kml',
+			 '/sample data/6800 E Tennessee Ave(4).kml',
 			 '/sample data/6800 E Tennessee Ave(5).kml',
 			 '/sample data/6800 E Tennessee Ave(6).kml',
 			 '/sample data/6800 E Tennessee Ave(7).kml']
-	}
+	},
 	server:{
 	    //ajax functions for phoning home
 	}
