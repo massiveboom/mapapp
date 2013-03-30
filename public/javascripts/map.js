a819f8f392f89ee42ae35dbcd56cb2f5fd23c577
devnill <devnill@gmail.com>
2013-03-30 02:06:08 -0600

    it's alive

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,13 +1,37 @@
+console.log('hi.');
 var map={
-    init:function(){},
+    init:function(){
+	var latlng = new google.maps.LatLng(51.764696,5.526042);
+	// set direction render options
+	var rendererOptions = { draggable: true };
+	directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+	var myOptions = {
+	    zoom: 14,
+	    center: latlng,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    mapTypeControl: false
+	};
+	// add the map to the map placeholder
+	var map = new google.maps.Map(document.getElementById("map"),myOptions);
+	directionsDisplay.setMap(map);
+	directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+	// Add a marker to the map for the end-point of the directions.
+	var marker = new google.maps.Marker({
+	    position: latlng, 
+	    map: map, 
+	    title:"Rodderhof, Oss"
+	}); 
+    },
     utils:{//commonly repeated code
 	
     },
     canvas:{
 	//the actual map
+	
     },
     geoip:{
 	//geolocation functions
+	
     },
     markers:{
 	//markers on the map
@@ -15,16 +39,22 @@ var map={
     },
     route:{
 	//routes on the map
-	init:function(){
-	}
+
     },
     prefs:{
 	//variables that store preferences
-    }
+
+    },
     display:{
 	//functions that augment the layout
+
     },
     server:{
 	//ajax functions for phoning home
     }
 }
+
+$(document).ready(function(){
+    console.log('starting');
+    map.init();
+});
