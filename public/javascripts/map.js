a4377205a1ea1c1a9958d7649da7f67720193cb7
devnill <devnill@gmail.com>
2013-03-30 14:45:24 -0600

    indecision

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,53 +1,57 @@
-var map={
-    init:function(){
-	var mapOptions = {
-	    center: new google.maps.LatLng(37.09024, -95.712891),
-	    zoom: 4,
-	    mapTypeId: google.maps.MapTypeId.TERRAIN
-	};
-	map.canvas = new google.maps.Map(document.getElementById("map"),map.prefs.mapOptions);
-    },
-    utils:{//commonly repeated code
+$(document).ready(function(){
+    var map={
+	init:function(){
+	    var mapOptions = {
+		center: new google.maps.LatLng(37.09024, -95.712891),
+		zoom: 4,
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	    };
+	    map.canvas = new google.maps.Map(document.getElementById("map"),map.prefs.mapOptions);
+	    map.geocoder = new google.maps.Geocoder();
+	    //event listeners go here
+	    $('body').on('click',function(event){
+		console.log('clicked');
+		console.log(event);
+	    });
 	
-    },
-    geo:{
-	//geolocation functions
 	
-    },
-    markers:{
-	//markers on the map
-	//add marker
-	//remove marker
-	
-    },
-    route:{
-	//routes on the map
-	//create path 
-    },
-    prefs:{
-	//variables that store preferences
-	mapOptions : {
-	    zoom: 4,
-   	    center: new google.maps.LatLng(37.09024, -95.712891),
-   	    mapTypeId: google.maps.MapTypeId.TERRAIN
-   	}
-    },
-    listeners:{
-	//event listeners go here
-    },
-    display:{
-	//functions that augment the layout
-	init:function(callback){
-	    //$("#map").css("width",($(window).width())+"px");
-	    if(callback){
-		console.log('yo dawg');
-		callback();
+	},
+	utils:{//commonly repeated code
+	    
+	},
+	markers:{
+	    //markers on the map
+	    //add marker
+	    //remove marker
+	    
+	},
+	route:{
+	    //routes on the map
+	    //create path 
+	},
+	prefs:{
+	    //variables that store preferences
+	    mapOptions : {
+		zoom: 4,
+   		center: new google.maps.LatLng(37.09024, -95.712891),
+   		mapTypeId: google.maps.MapTypeId.TERRAIN
+   	    }
+	},
+	display:{
+	    //functions that augment the layout
+	    init:function(callback){
+		//$("#map").css("width",($(window).width())+"px");
+		if(callback){
+		    console.log('yo dawg');
+		    callback();
+		}
 	    }
+	},
+	server:{
+	    //ajax functions for phoning home
 	}
-    },
-    server:{
-	//ajax functions for phoning home
     }
-}
+});
+
 
 google.maps.event.addDomListener(window, 'load', map.init);
