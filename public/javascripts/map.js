41cc67cf29c3cee622c32bbcacf82ccacc3bac51
AtomicFiredoll <atomicfiredoll@usebombs.com>
2013-03-30 19:53:03 -0600

    rework add mark, start remove

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,20 +4,19 @@ $(document).ready(function(){
 	    //load api resources
 	    map.canvas = new google.maps.Map(document.getElementById("map"),map.prefs.mapOptions);
 	    map.geocoder = new google.maps.Geocoder();
-           
-            map.listener.mapClick = new google.maps.event.addListener(map.canvas, 'click', function(e) {
+		
+			map.listener.mapClick = new google.maps.event.addListener(map.canvas, 'click', function(e) {
 		console.log(['click',e]);
 	    });
-            
+
+		//test markers
+		var testlocation = new google.maps.LatLng(37.7699298, -122.4469157);
+		map.markers.addMarker(testlocation,"testmark");
+
 	    //event listeners go here
 	    $('body').on('click',function(event){
 		console.log('clicked');
 		console.log(event);
-		//test markers
-		var testlocation = new google.maps.LatLng(37.7699298, -122.4469157);
-		console.log("test1");
-		map.markers.addMarker(testlocation);
-		console.log("test2");
 	    });	
 	},
 	listener:{},
@@ -40,20 +39,28 @@ $(document).ready(function(){
             
 	},
 	markers:{
-	    //markers on the map
-	    //add marker
-	    //remove marker
-	    addMarker:function(location){
-		console.log("fuckall");
-		var testmarker = new google.maps.Marker(
-		    {
-			position: location,
-			map: map.canvas,
-			draggable: true
-		    }
+		//markers on the map
+		//add marker
+		//remove marker
+		store: [],
+		addMarker:function(location, name){
+		map.marker.store[name] = new google.maps.Marker(
+			{
+			position: location
+			,map: map.canvas
+			,draggable: true
+			}
 		);
-	    },
-		removeMarker:function(){},
+		console.log(map.markers.store);
+		},
+		removeMarker:function(targetMarker){
+			for(i in markers.store){ 
+				if (i == targetMarker) {
+					markers.store[i].setMap(null);
+					markers.store[i].splice(0,1);
+				};
+			}
+		},
 	},
 	route:{
 	    //routes on the map
