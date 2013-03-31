3b00ab59d81889ef470783c514019e3d7dd45adc
devnill <devnill@gmail.com>
2013-03-30 20:15:04 -0600

    routing

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,33 +4,35 @@ $(document).ready(function(){
 	    //load api resources
 	    map.canvas = new google.maps.Map(document.getElementById("map"),map.prefs.mapOptions);
 	    map.geocoder = new google.maps.Geocoder();
+            map.route.service = new google.maps.DirectionsService();
+            map.route.display = new google.maps.DirectionsRenderer(map.prefs.routeOptions);
+            map.route.display.setMap(map.canvas);
+            map.route.display.setPanel(map.data.route);
 
             map.listener.mapClick = new google.maps.event.addListener(map.canvas, 'click', function(e) {
 
 		console.log(['click',e]);
+	    });	    
+	    map.listener.uiClick = $('body').on('click', '#buttons a', function(){
+		switch($(this).attr('id'))
+		{
+		case "next":
+		    map.utils.kml.load(map.prefs.sampleData[2]);
+		    break;
+		    
+		case "previous":
+		    
+		    break;
+		    
+		case "all":
+		    map.utils.kml.load(map.prefs.sampleData);
+		    break;
+		    
+		case "clear":
+		    map.utils.kml.clear(map.prefs.sampleData);
+		    break;
+		}
 	    });
-	    
-	    map.listener.uiClick = $('#buttons').on('click', 'a', function()
-	    {
-			switch($(this).attr('id'))
-			{
-				case "next":
-					map.utils.kml.load(map.prefs.sampleData[2]);
-					break;
-					
-				case "previous":
-				
-					break;
-					
-				case "all":
-					map.utils.kml.load(map.prefs.sampleData);
-					break;
-					
-				case "clear":
-					map.utils.kml.clear(map.prefs.sampleData);
-					break;
-			}
-		});
             
 	    //event listeners go here
 	    $('body').on('click',function(event){
@@ -46,19 +48,18 @@ $(document).ready(function(){
 	overlays:{},
 	listener:{},
 	utils:{//commonly repeated code
-		kml:
+	    kml:
+	    {
+		load: function(data)
 		{
-			load: function(data)
-			{
-				if(typeof(data) === 'string')
-				{
-					map.overlays[data] = new google.maps.KmlLayer(data);
-					map.overlays[data].setMap(map.canvas);
-				}
-			},
-			
-			clear: function() {},
+		    if(typeof(data) === 'string')
+		    {
+			map.overlays[data] = new google.maps.KmlLayer(data);
+			map.overlays[data].setMap(map.canvas);
+		    }
 		},
+		clear: function() {},
+	    },
             encodeAddress:function(location,callback){
                 map.geocoder.geocode( { 'address': location}, function(results, status) {
       		    if(status != google.maps.GeocoderStatus.OK){
@@ -70,9 +71,19 @@ $(document).ready(function(){
                     }
                 });
             }
-            calcRoute: function(){
-
-
+            calcRoute: function(waypoints){
+                if(waypoints.length>1){
+                    var origin,destination;
+                    origin=waypoints.splice(0,1);
+                    destination=waypoints.pop();
+                    var req = {
+                        origin: origin,
+                        destination: destination,
+                        waypoints: waypoints,
+                        unitSystem: google.maps.UnitSystem.IMPERIAL,
+                        travelMode: google.maps.DirectionsTravelMode[map.prefs.travelMode]
+                    };
+                }
             }            
 	},
 	markers:{
@@ -92,16 +103,16 @@ $(document).ready(function(){
 		removeMarker:function(){},
 	},
 	route:{
-	    //routes on the map
-	    //create path 
 	},
 	prefs:{
 	    //variables that store preferences
+            travelMode : 'BICYCLING',
 	    mapOptions : {
 		zoom: 4,
 		center: new google.maps.LatLng(37.09024, -95.712891),
 		mapTypeId: google.maps.MapTypeId.TERRAIN
 	    },
+            routeOptions = { draggable: true };
 	    sampleData: ['/sample data/6800 E Tennessee Ave.kml',
 			 '/sample data/6800 E Tennessee Ave(2).kml',
 			 '/sample data/6800 E Tennessee Ave(3).kml',
@@ -112,7 +123,10 @@ $(document).ready(function(){
 	},
 	server:{
 	    //ajax functions for phoning home
-	}
+	},
+        data:{
+            //specific data for routes, etc
+        }
     }
     google.maps.event.addDomListener(window, 'load', map.init);
 });
